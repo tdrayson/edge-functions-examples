8f04b3d4e57d72c8c93b84c818136ec3bc1f442c
Taylor Drayson <taylordrayson@taylors-mbp.home>
2022-04-19 17:32:02 +0100

    Added random quote

---
+++ netlify/edge-functions/randomQuote.js
@@ -0,0 +1,12 @@
+import { Context } from "netlify:edge";
+
+export default async (Request,Context) => {
+
+  const quote = await fetch("https://zenquotes.io/api/random", {
+    "headers": {
+      "Accept": "application/json"
+    }
+  });
+  const jsonData = await quote.json();
+  return context.json(jsonData);
+};
